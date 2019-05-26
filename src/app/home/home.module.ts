53991d05a8c59b7de1b97fc8cc8159226853ed81
Achyudh Ram <achyudhk@gmail.com>
2019-05-26 06:39:42 -0400

    Add ImageContainer component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -8,7 +8,8 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { ImageService } from './image.service';
 import { ThemeModule } from '../@theme/theme.module';
-import { GalleryComponent } from './gallery/gallery.component'
+import { GalleryComponent } from './gallery/gallery.component';
+import { ImageContainerComponent } from './gallery/image-container/image-container.component'
 
 @NgModule({
   imports:
@@ -20,7 +21,7 @@ import { GalleryComponent } from './gallery/gallery.component'
     HomeRoutingModule,
     ThemeModule
   ],
-  declarations: [HomeComponent, GalleryComponent],
+  declarations: [HomeComponent, GalleryComponent, ImageContainerComponent],
   providers: [ImageService]
 })
 export class HomeModule {}
