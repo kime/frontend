07d75752a822522e2d712375a2b18520208a8248
Achyudh Ram <achyudhk@gmail.com>
2019-05-28 00:26:49 -0400

    Fix tests for FooterComponent

--- src/app/shell/header/header.component.spec.ts
+++ src/app/shell/header/header.component.spec.ts
@@ -1,6 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { TranslateModule } from '@ngx-translate/core';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { ThemeModule } from '../../@theme/theme.module';
@@ -17,8 +16,7 @@ describe('HeaderComponent', () => {
       [
         ThemeModule,
         RouterTestingModule, 
-        NgbModule, 
-        TranslateModule.forRoot()
+        NgbModule 
       ],
       declarations: [HeaderComponent],
       providers: [{ provide: AuthenticationService, useClass: MockAuthenticationService }]
