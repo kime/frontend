0929322453739b1d70b42bdc0862fc17638b2e0c
Achyudh Ram <achyudhk@gmail.com>
2019-05-29 04:43:43 -0400

    Update proxy.conf.js with local dev server

--- proxy.conf.js
+++ proxy.conf.js
@@ -4,15 +4,13 @@ const HttpsProxyAgent = require('https-proxy-agent');
 
 /*
  * API proxy configuration.
- * This allows you to proxy HTTP request like `http.get('/api/stuff')` to another server/port.
- * This is especially useful during app development to avoid CORS issues while running a local server.
- * For more details and options, see https://github.com/angular/angular-cli#proxy-to-backend
+ * This helps avoid CORS issues while running a local server during development.
  */
 const proxyConfig = [
   {
     context: '/api',
     pathRewrite: { '^/api': '' },
-    target: 'https://api.chucknorris.io',
+    target: 'http://0.0.0.0:3780/api',
     changeOrigin: true,
     secure: false
   }
