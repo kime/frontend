03c0ac8d4d54e45f7d5fcc23ec9339ff5d064eed
Achyudh Ram <achyudhk@gmail.com>
2019-06-05 04:06:28 -0400

    Add EnhanceRequestContext to interfaces

--- src/app/shared/interfaces.ts
+++ src/app/shared/interfaces.ts
@@ -16,3 +16,21 @@ export class ImageContext {
   originalImage: Image;
   enhancedImage?: EnhancedImage;
 }
+
+export class EnhanceRequestContext {
+  id: number;
+  name: string;
+  uploaded: string;
+  multiplier: number;
+  fixArtifacts: boolean;
+  originalImage: Image;
+
+  constructor(context: ImageContext, multiplier: number, fixArtifacts: boolean) {
+    this.id = context.id;
+    this.name = context.name;
+    this.uploaded = context.uploaded;
+    this.originalImage = context.originalImage;
+    this.multiplier = multiplier;
+    this.fixArtifacts = fixArtifacts;
+  }
+}
