7a4917878643f2bb3d8744b01ebad85a2899c6c0
Achyudh Ram <achyudhk@gmail.com>
2019-06-08 03:53:26 -0400

    Add signup form in the login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { finalize } from 'rxjs/operators';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { finalize, flatMap } from 'rxjs/operators';
 
 import { environment } from '@env/environment';
 import { Logger, AuthenticationService } from '@app/core';
@@ -15,9 +15,17 @@ const log = new Logger('Login');
 })
 export class LoginComponent implements OnInit {
   version: string = environment.version;
-  error: string;
+  loginError: string;
+  signupError: string;
   loginForm: FormGroup;
+  signupForm: FormGroup;
   isLoading = false;
+
+  private static checkPasswords(control: AbstractControl) {
+    if (control.get('password').value !== control.get('confirmPassword').value) {
+      return {invalid: true};
+    }
+  }
 
   constructor(
     private router: Router,
@@ -28,7 +36,8 @@ export class LoginComponent implements OnInit {
     this.createForm();
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
   login() {
     this.isLoading = true;
@@ -49,16 +58,82 @@ export class LoginComponent implements OnInit {
         },
         error => {
           log.debug(`Login error: ${error}`);
-          this.error = error;
+          this.loginError = error;
         }
       );
+  }
+
+  signup() {
+    this.isLoading = true;
+    this.authenticationService
+      .signup(this.signupForm.value)
+      .pipe(
+        finalize(() => {
+          this.signupForm.markAsPristine();
+          this.isLoading = false;
+        })
+      )
+      .pipe(
+        flatMap( () => {
+         return this.authenticationService.login(this.signupForm.value);
+        })
+      )
+      .subscribe(
+        credentials => {
+          log.debug(`${credentials.username} successfully signed up`);
+          this.route.queryParams.subscribe(params =>
+            this.router.navigate([params.redirect || '/'], { replaceUrl: true })
+          );
+        },
+        error => {
+          log.debug(`Signup error: ${error}`);
+          this.signupError = error;
+        }
+      );
+  }
+
+  getSignupStatus() {
+    if (this.signupError) {
+      return 'warning';
+    } else {
+      return '';
+    }
+  }
+
+  getLoginStatus(): string {
+    if (this.loginError) {
+      return 'warning';
+    } else {
+      return '';
+    }
+  }
+
+  getSignupHeader(): string {
+    if (this.signupError) {
+      return 'Username already exits!';
+    } else {
+      return 'Create an account';
+    }
+  }
+
+  getLoginHeader(): string {
+    if (this.loginError) {
+      return 'Incorrect username or password!';
+    } else {
+      return 'Sign in to your account';
+    }
   }
 
   private createForm() {
     this.loginForm = this.formBuilder.group({
       username: ['', Validators.required],
       password: ['', Validators.required],
-      remember: true
     });
+
+    this.signupForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    }, {validator: LoginComponent.checkPasswords});
   }
 }
